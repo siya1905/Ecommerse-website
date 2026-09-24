66b367506178248582db9538b58227821b53f813
siya1905 <siyasachan304@gmail.com>
2026-09-24 12:08:26 +0530

    title change of home page

--- src/pages/HeroPage.jsx
+++ src/pages/HeroPage.jsx
@@ -7,7 +7,7 @@ function HeroPage() {
 
   return (
     <>
-     <h2 className=" flex items-center justify-center text-8xl font-bold hover:text-green-400-600 ">first page of this website </h2>
+     <h2 className=" flex items-center justify-center text-8xl font-bold hover:text-red-400-600 ">home page of this website  </h2>
     </>
   );
 }
